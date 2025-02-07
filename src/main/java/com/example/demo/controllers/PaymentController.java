aecddfc100e84286b6648235bd2c1715ade34129
Choduboina Vishnu Yadhav <162417330+Vishnu-Yadhav@users.noreply.github.com>
2025-02-07 13:39:24 +0530

    07/02/2025
    Updated

---
+++ src/main/java/com/example/demo/controllers/PaymentController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@RequestMapping("/api/payment")
+public class PaymentController {
+	@GetMapping("/doPayment")
+	public String doPayment() {
+		return "Payment Succcess";
+	}
+}
